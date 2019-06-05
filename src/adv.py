317e7c5f3950310bdc0eab3b399fc9b5f90e3fef
jmendez44 <jenuel.mendez@gmail.com>
2019-06-05 19:07:36 -0400

    working on main loop

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,7 @@
 from room import Room
+from player import Player
+
+import sys
 
 # Declare all the rooms
 
@@ -39,6 +42,8 @@ room['treasure'].s_to = room['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 
+player = Player(room['outside'], [])
+
 # Write a loop that:
 #
 # * Prints the current room name
@@ -49,3 +54,6 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+def check_move(direction, room):
+    attribute = direction + '_to'
